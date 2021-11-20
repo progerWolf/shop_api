4fe0e0a467260377fdc4c3922bbdb3e1b9f1c413
unknown <dalersho.rm@yandex.ru>
2021-11-20 15:58:09 +0500

    main page

--- frontend/components/Starting/Navbar.vue
+++ frontend/components/Starting/Navbar.vue
@@ -1,30 +1,24 @@
 <template>
     <div class="border-b-2">
         <div class="container mx-auto">
-            <div class="w-full flex items-center justify-between">
-                <div >
-                    <img src="/logo.svg" width="160" alt="">
+            <div class="w-full flex items-center justify-between py-3">
+                <div class="flex items-center">
+                    <img src="/logo.png" class="rounded-full" width="64px" alt="">
+                    <ul class="flex">
+                        <li class="ml-2">
+                            <a href="#" class="ml-6 flex">
+                                <svg class="text-brand" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="24"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 9.75a2.628 2.628 0 01-2.625-2.625A2.628 2.628 0 019 4.5a2.628 2.628 0 012.625 2.625A2.628 2.628 0 019 9.75zM9 1.5c-3.308 0-6 2.665-6 5.942 0 4.106 5.287 8.684 5.512 8.877a.748.748 0 00.976 0C9.713 16.127 15 11.548 15 7.442 15 4.165 12.308 1.5 9 1.5z"></path></svg>
+                                <span>Душанбе</span>
+                            </a>
+                        </li>
+                        <li class="ml-2">
+                            <nuxt-link class="rounded-md px-3 py-2 hover:bg-brand hover:text-white" to="">Магазин</nuxt-link>
+                        </li>
+                    </ul>
                 </div>
-                <div>
-                    <a href="#" class="mx-2">Категории</a>
-                </div>
-                <form class="relative w-full mx-3">
-                    <input type="text" class="w-full rounded-md border-2 border-gray-700 px-4 py-2" placeholder="Найдите свой курс">
-                    <button class="absolute top-3 right-3">
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M19 19L13 13M15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z" stroke="#1E1718" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>
-                </form>
                 <div class="flex mx-3">
-                    <nuxt-link class="mx-1 border-2 rounded-md px-6 py-2 border-z-orange text-z-orange hover:bg-z-orange hover:text-white" to="">Войти</nuxt-link>
-                    <nuxt-link class="mx-1 border-2 rounded-md px-3 py-2 bg-z-orange border-z-orange text-white" to="">Регистрация</nuxt-link>
-                    <div class="flex items-center mx-1 border-2 border-gray-700 rounded-md px-3 py-2">
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M19 10C19 12.3869 18.0518 14.6761 16.364 16.364C14.6761 18.0518 12.3869 19 10 19M19 10C19 7.61305 18.0518 5.32387 16.364 3.63604C14.6761 1.94821 12.3869 1 10 1M19 10H1M10 19C7.61305 19 5.32387 18.0518 3.63604 16.364C1.94821 14.6761 1 12.3869 1 10M10 19C11.657 19 13 14.97 13 10C13 5.03 11.657 1 10 1M10 19C8.343 19 7 14.97 7 10C7 5.03 8.343 1 10 1M10 1C7.61305 1 5.32387 1.94821 3.63604 3.63604C1.94821 5.32387 1 7.61305 1 10" stroke="#1E1718" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                        <span class="ml-2">Языки</span>
-                    </div>
+                    <nuxt-link class="mx-1 border-2 rounded-md px-6 py-2 border-brand text-brand hover:bg-brand hover:text-white" to="">Заказать доставку</nuxt-link>
+                    <nuxt-link class="mx-1 border-2 rounded-md px-3 py-2 bg-brand border-brand text-white" to="">Вход / Регистрация</nuxt-link>
                 </div>
             </div>
         </div>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="container mx-auto">
-    <div class="w-full flex items-start my-12">
+    <div class="w-full flex items-center my-12">
       <div class="w-1/2">
-        <h1 class="mb-8 text-z-orange font-bold text-[64px] leading-[100%]">Учи таджикский ультуру  таджикского народа </h1>
+        <h1 class="mb-8 text-brand font-bold text-[48px] leading-[100%]">Срочная доставка по РТ</h1>
         <p class="mb-8 text-gray-500 text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
         <div>
-         <nuxt-link class="border-2 rounded-md px-12 py-4 bg-z-orange border-transparent focus:outline-none text-white text-lg focus:border-red-300" to="">Стать студентом</nuxt-link>
+         <nuxt-link class="border-2 rounded-md px-12 py-4 bg-brand border-transparent focus:outline-none text-white text-lg focus:border-red-300" to="">Заказать доставку</nuxt-link>
         </div>
       </div>
       <div class="w-1/2 h-full">
-        <img src="/header.svg" alt="">
+        <form class="rounded-lg bg-gray-100 p-6 w-full lg:w-10/12 xl:w-2/3">
+          <div class="relative">
+            <input type="text" class="form-control py-2 pl-8 pr-3 text-gray-700" placeholder="Адрес отправки">
+            <svg class="text-gray-700 absolute top-2 left-2" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="20"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 9.75a2.628 2.628 0 01-2.625-2.625A2.628 2.628 0 019 4.5a2.628 2.628 0 012.625 2.625A2.628 2.628 0 019 9.75zM9 1.5c-3.308 0-6 2.665-6 5.942 0 4.106 5.287 8.684 5.512 8.877a.748.748 0 00.976 0C9.713 16.127 15 11.548 15 7.442 15 4.165 12.308 1.5 9 1.5z"></path></svg>
+          </div>
+          <div class="relative mt-3">
+            <input type="text" class="form-control py-2 pl-8 pr-3 text-gray-700" placeholder="Адрес доставки">
+            <svg class="text-gray-700 absolute top-2 left-2" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="20"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 9.75a2.628 2.628 0 01-2.625-2.625A2.628 2.628 0 019 4.5a2.628 2.628 0 012.625 2.625A2.628 2.628 0 019 9.75zM9 1.5c-3.308 0-6 2.665-6 5.942 0 4.106 5.287 8.684 5.512 8.877a.748.748 0 00.976 0C9.713 16.127 15 11.548 15 7.442 15 4.165 12.308 1.5 9 1.5z"></path></svg>
+          </div>
+          <div class="flex justify-between mt-3">
+            <button type="button" class="tab active">
+              Пешком
+            </button>
+            <button type="button" class="tab">
+              Автомобилем
+            </button>
+            <button type="button" class="tab">
+              Грузовиком
+            </button>
+          </div>
+          <div class="mt-3">
+            Рассчитать стоимость
+          </div>
+          <div class="mt-3">
+            Заказ без регистрации, оплата по факту доставки
+          </div>
+        </form>
       </div>
     </div>
 
