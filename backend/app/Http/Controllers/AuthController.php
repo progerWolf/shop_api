4f7d9f878e2342cc5209779c700242eaa030e9b0
DRM <67089702+dalershorm@users.noreply.github.com>
2021-12-01 12:55:24 +0500

    get user info

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -299,7 +299,7 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function me()
+    public function userProfile()
     {
         return response()->json(auth()->user());
     }
