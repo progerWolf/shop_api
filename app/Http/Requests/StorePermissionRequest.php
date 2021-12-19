253249d5491c50ba98f3297a82c15c839ff40453
progerWolf <manalonewolf@gmail.com>
2021-12-19 12:27:04 +0300

    Added permission controlling functionality.

---
+++ app/Http/Requests/StorePermissionRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StorePermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'permission_type' => 'required|min:4|max:5'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'permission_type.required' => 'Тип является обязательным',
+            'permission_type.min' => 'Дина не дожна быть менее 4 символо',
+            'permission_type.max' => 'Дина не дожна быть более 5 символо',
+        ];
+    }
+
+    /**
+     * [failedValidation [Overriding the event validator for custom error response]]
+     * @param Validator $validator [description]
+     *
+     * @return void [object][object of various validation errors]
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
