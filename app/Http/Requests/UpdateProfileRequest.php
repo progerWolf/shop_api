684362c00b2f9a27cc7a03d423a0d87c6d861e92
progerWolf <manalonewolf@gmail.com>
2021-12-27 15:34:02 +0300

    Added filter.

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\Rule;
-use JetBrains\PhpStorm\ArrayShape;
 
 class UpdateProfileRequest extends FormRequest
 {
